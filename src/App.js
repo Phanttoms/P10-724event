5103d1ee17b91fa9e9ed24fb82330499b1992ccd
Phanttoms <yohan.pruche@orange.fr>
2024-04-10 11:44:33 +0200

    test

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import Page from "./pages/Home";
 import { DataProvider } from "./contexts/DataContext";
 
 function App() {
-  return (
-    <DataProvider>
-      <Page />
-    </DataProvider>
-  );
+	return (
+		<DataProvider>
+			<Page />
+		</DataProvider>
+	);
 }
 
 export default App;
